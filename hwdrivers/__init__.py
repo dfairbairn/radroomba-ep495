a439dab093a7b4188fc21766dcfcafad57300f59
radroomba <dtf831@mail.usask.ca>
2017-03-10 13:58:26 -0600

    Hardware drivers in repository now

---
+++ hwdrivers/__init__.py
@@ -0,0 +1,14 @@
+"""
+Sub-Package: Hardware Drivers
+    This package will contain the drivers for some of the various pieces
+    of hardware used for the CurieUS robot, including:
+    - BNO055 IMU unit driver
+    - DUal MC33926 Motor Driver
+    - Stepper Motor Driver Code (adapted from Dual MC33926)
+    - ADC code??
+
+"""
+
+import BNO055
+import dual_mc33926_rpi_4raster
+import dual_mc33926_rpi
